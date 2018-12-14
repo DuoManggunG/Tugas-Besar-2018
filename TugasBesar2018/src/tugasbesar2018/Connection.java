33a4aa7c617b00071990b785139d08e80d8b0362
HP <HP@R1o-mu5>
2018-12-14 15:15:52 +0700

    membuat class Connection

---
+++ TugasBesar2018/src/tugasbesar2018/Connection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasbesar2018;
+
+/**
+ *
+ * @author HP
+ */
+public class Connection {
+    
+}
