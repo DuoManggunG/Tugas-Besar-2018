cab4070ac8d82e284703a612198b8f0dbd4b3c34
TikaYulianti <tikayulian27@gmail.com>
2018-12-14 15:44:09 +0700

    membuat Class Connection

--- TugasBesar2018/src/tugasbesar2018/Connection.java
+++ TugasBesar2018/src/tugasbesar2018/Connection.java
@@ -5,10 +5,44 @@
  */
 package tugasbesar2018;
 
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author HP
  */
 public class Connection {
     
+    private Connection connection;
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    //untuk koneksi ke database
+    public void dbCOnnection(){
+        try{
+            Class.forName("");//mus
+            
+            //cek Database
+            try{
+            String url, user, password;
+            url="";//mus
+            user="root";
+            password="";
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Koneksi Sukses");
+            
+            }catch (SQLException se){
+                JOptionPane.showMessageDialog(null, "Konksi Gagal!"+ se);
+                System.exit(0);
+            }catch (ClassNotFoundException cnfe){
+                JOptionPane.showMessageDialog(null, "Driver Tidak Ditemukan!" + cnfe);
+                System.exit(0);
+            }
+        }
+        public static void main(String[] kon) {
+        new Koneksi().dbConnection();
+    }
 }
